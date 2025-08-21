ec0e8e3d7a3400266074261083ed7c20980ddd04
WesleySousaC <wesley.aika1@gmail.com>
2025-08-20 21:05:57 -0300

    Add files via upload

---
+++ Calculadora.py
@@ -0,0 +1,27 @@
+num1=int(input("digite um numero:"))
+num2=int(input("digite outro numero:")) 
+
+soma=num1+num2
+subtração=num1-num2
+divisão=num1/num2
+multiplicação=num1*num2
+
+print(soma)
+print(subtração)
+print(divisão)
+print(multiplicação)
+
+# input subtrair (a, b):
+    
+
+# input multiplicar (a, b):
+
+
+    
+#      calculadora():
+#         print("Selecione a operação:")
+#         print("'. Soma")
+#         print("2. Subtração")
+#         print("3. Multiplicação")
+#         print("4. Divisão")
+#         print("0.. Sair")
